6c38f49e8b84df0124e4ca2639b659719a12f98e
BeeBoy (James Flynn) <jflynn129@gmail.com>
2018-11-06 11:08:50 -0500

    Update mon.sh
    Updated to use Azure CLI

--- mon.sh
+++ mon.sh
@@ -1,1 +1,3 @@
-iothub-explorer monitor-events  STL496ZG-BG96 --login "HostName=XXXX;SharedAccessKeyName=XXXX;SharedAccessKey=XXXX"
+#iothub-explorer monitor-events  STL496ZG-BG96 --login "HostName=XXXX;SharedAccessKeyName=XXXX;SharedAccessKey=XXXX"
+#
+az iot hub monitor-events --login "HostName=XXXX;SharedAccessKeyName=XXXX;SharedAccessKey=XXXX"
