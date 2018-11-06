82d6ceb113126a357661515ba73cbee182ab2e5b
BeeBoy (James Flynn) <jflynn129@gmail.com>
2018-11-06 11:09:43 -0500

    Update send.sh
    Updated to use Azure CLI

--- send.sh
+++ send.sh
@@ -1,1 +1,3 @@
-iothub-explorer send STL496ZG-BG96 $* --login "HostName=XXXX;SharedAccessKeyName=iothubowner;SharedAccessKey=XXXX"
+#iothub-explorer send STL496ZG-BG96 $* --login "HostName=XXXX;SharedAccessKeyName=iothubowner;SharedAccessKey=XXXX"
+#
+az iot device c2d-message send --login "HostName=XXX;SharedAccessKeyName=XXX;SharedAccessKey=XXX" --device-id XXX --data "$*"
